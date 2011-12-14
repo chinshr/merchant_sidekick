1729656ea73247ba4ca5a3807cec2f0cf549836e
Norman Clarke <norman@njclarke.com>
2011-12-14 16:37:32 -0300

    Improve AR setup

--- spec/new_spec_helper.rb
+++ spec/new_spec_helper.rb
@@ -4,12 +4,18 @@ require "active_record"
 require "sqlite3"
 require "merchant_sidekick"
 
+# If you want to see the ActiveRecord log, invoke the tests using `rake test LOG=true`
+if ENV["LOG"]
+  require "logger"
+  ActiveRecord::Base.logger = Logger.new($stdout)
+end
+
 ActiveRecord::Base.establish_connection :adapter => "sqlite3", :database => ":memory:"
+ActiveRecord::Migration.verbose = false
 
 require "merchant_sidekick/migrations/billing"
 require "merchant_sidekick/migrations/addressable"
 require "merchant_sidekick/migrations/shopping_cart"
-
 
 CreateMerchantSidekickBillingTables.up
 CreateMerchantSidekickAddressableTables.up
