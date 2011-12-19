428df1839cd1aead98572f07021f3011ebb7f191
Juergen Fesslmeier <jfesslmeier@gmail.com>
2011-12-19 15:31:01 -0300

    Fixing payment spec

--- spec/payment_spec.rb
+++ spec/payment_spec.rb
@@ -3,7 +3,7 @@ require File.expand_path("../spec_helper", __FILE__)
 describe "A payment" do
 
   it "should belong to a payable" do
-    lambda { MerchantSidekick::Payments::Payment.new.payable(true) }.should_not raise_error
+    lambda { MerchantSidekick::Payments::Payment.new.should respond_to(:payable) }.should_not raise_error
   end
 
   it "should have an amount" do
