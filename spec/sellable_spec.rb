1df322a2d6373dcc029b51952895fb7b930ba176
Juergen Fesslmeier <jfesslmeier@gmail.com>
2011-12-21 18:07:37 -0300

    Adding specs for models with money

--- spec/sellable_spec.rb
+++ spec/sellable_spec.rb
@@ -1,9 +1,9 @@
 require File.expand_path("../spec_helper", __FILE__)
 
-describe "A sellable" do
+describe "A sellable model" do
 
   it "should have a price" do
-    lambda { Product.new.price }.should_not raise_error
+    lambda { Product.new.should.respond_to?(:price) }.should_not raise_error
   end
   
   it "should have many line_items" do
@@ -12,6 +12,23 @@ describe "A sellable" do
   
   it "should have many orders" do
     lambda { Product.new.orders.should == [] }.should_not raise_error
+  end
+  
+end
+
+describe "A sellable with money" do
+
+  it "should have a currency accessor" do
+    lambda {
+      Product.new.should.respond_to?(:price_cents)
+      Product.new.should.respond_to?(:price_currency)
+      Product.new.price_currency.should == "USD"
+      Product.new.currency.should == ::Money::Currency.wrap("USD") 
+    }.should_not raise_error
+  end
+
+  it "should have a currency as string accessor" do
+    lambda { Product.new.currency_as_string.should == "USD" }.should_not raise_error
   end
   
 end
