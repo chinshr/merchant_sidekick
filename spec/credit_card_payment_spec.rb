44c2610c10c262c5b6e36cbe6fc93e714a3d934e
Juergen Fesslmeier <jfesslmeier@gmail.com>
2011-12-27 10:02:23 -0300

    Moving credit card payment to spec

---
+++ spec/credit_card_payment_spec.rb
@@ -0,0 +1,134 @@
+require File.expand_path("../spec_helper", __FILE__)
+
+describe MerchantSidekick::Payments::CreditCardPayment, "authorization" do
+  
+  def setup
+    @amount = Money.new(100, 'USD')
+  end
+  
+  it "should succeed" do
+    transaction do
+      auth = MerchantSidekick::Payments::CreditCardPayment.authorize(
+        @amount,
+        credit_card(valid_credit_card_attributes)
+      )
+      auth.success.should == true
+      auth.action.should == 'authorization'
+      auth.message.should == ActiveMerchant::Billing::BogusGateway::SUCCESS_MESSAGE
+      auth[:reference].should == ActiveMerchant::Billing::BogusGateway::AUTHORIZATION
+    end
+  end
+  
+  it "should fail" do
+    transaction do
+      auth = MerchantSidekick::Payments::CreditCardPayment.authorize(
+        @amount,
+        credit_card(invalid_credit_card_attributes)
+      )
+      auth.success.should == false
+      auth.action.should == 'authorization'
+      auth.message.should == ActiveMerchant::Billing::BogusGateway::FAILURE_MESSAGE
+    end
+  end
+  
+  it "should error" do
+    transaction do
+      auth = MerchantSidekick::Payments::CreditCardPayment.authorize(
+        @amount,
+        credit_card(valid_credit_card_attributes(:number => '3'))
+      )
+      auth.success.should == false
+      auth.action.should == 'authorization'
+      auth.message.should == ActiveMerchant::Billing::BogusGateway::ERROR_MESSAGE
+    end
+  end
+  
+end
+
+describe MerchantSidekick::Payments::CreditCardPayment, "capture" do
+  
+  def setup
+    @amount = Money.new(100, 'USD')
+  end
+  
+  it "should capture successfully" do
+    transaction do
+      capt = MerchantSidekick::Payments::CreditCardPayment.capture(
+        @amount,
+        '123'
+      )
+      capt.success.should == true
+      capt.action.should == 'capture'
+      capt.message.should == ActiveMerchant::Billing::BogusGateway::SUCCESS_MESSAGE
+    end
+  end
+  
+  it "should fail capture" do
+    transaction do
+      capt = MerchantSidekick::Payments::CreditCardPayment.capture(
+        @amount,
+        '2'
+      )
+      capt.success.should == false
+      capt.action.should == 'capture'
+      capt.message.should == ActiveMerchant::Billing::BogusGateway::FAILURE_MESSAGE
+    end
+  end
+  
+  it "should error capture" do
+    transaction do
+      capt = MerchantSidekick::Payments::CreditCardPayment.capture(
+        @amount,
+        '1'
+      )
+      capt.success.should == false
+      capt.action.should == 'capture'
+      capt.message.should == ActiveMerchant::Billing::BogusGateway::CAPTURE_ERROR_MESSAGE
+    end
+  end
+  
+end
+
+describe MerchantSidekick::Payments::CreditCardPayment, "transfer method" do
+  
+  def setup
+    @amount = Money.new(100, 'USD')
+  end
+  
+  it "should sucessfully transfer" do
+    transaction do
+      capt = MerchantSidekick::Payments::CreditCardPayment.transfer(
+        @amount,
+        'account@test.tst'
+      )
+      capt.success.should == true
+      capt.action.should == 'transfer'
+      capt.message.should == ActiveMerchant::Billing::BogusGateway::SUCCESS_MESSAGE
+    end
+  end
+
+  it "should return error" do
+    transaction do
+      capt = MerchantSidekick::Payments::CreditCardPayment.transfer(
+        @amount,
+        'error@error.tst'
+      )
+      capt.success.should == false
+      capt.action.should == 'transfer'
+      capt.message.should == ActiveMerchant::Billing::BogusGateway::ERROR_MESSAGE
+    end
+  end
+
+  it "should fail" do
+    transaction do
+      capt = MerchantSidekick::Payments::CreditCardPayment.transfer(
+        @amount,
+        'fail@error.tst'
+      )
+      capt.success.should == false
+      capt.action.should == 'transfer'
+      capt.message.should == ActiveMerchant::Billing::BogusGateway::FAILURE_MESSAGE
+    end
+  end
+  
+end
